60bf51b5c7b5016221a1e862b01dcf5c370dd28e
Rizky Al Ikhsan <160478961+Jisatsu092@users.noreply.github.com>
2025-05-19 23:48:20 +0700

    Initial commit for To-Do List app

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkSUxHbiY4LqRZcvZwdYFYqiiHLxCAJ1M",
+    authDomain: "to-do-list-8732d.firebaseapp.com",
+    projectId: "to-do-list-8732d",
+    storageBucket: "to-do-list-8732d.firebasestorage.app",
+    messagingSenderId: "906369083264",
+    appId: "1:906369083264:web:637bd181ca27eee6338365",
+    measurementId: "G-5LWETTQHFK"
+};
+
+const emailJsUserId = "-X3AO9JdqzEb8dZIC";
